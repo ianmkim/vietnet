b986e94d591e25b5c2fae02bb40cb397a45d194d
Ian Kim <ian@ianmkim.com>
2022-01-06 09:00:27 -0500

    latest commit for reed graff

--- SNN.py
+++ SNN.py
@@ -156,3 +156,27 @@ class SpikingNet(nn.Module):
         plt.xlabel("Time")
         plt.ylabel("Neuron's activation")
         plt.show()
+
+if __name__ == "__main__":
+    from util import download_mnist
+    from util import test, train, train_many_epochs
+    from SNN import SpikingNet
+
+    batch_size = 1000
+    DATA_PATH = './data'
+
+    training_set, testing_set = download_mnist(DATA_PATH)
+    train_set_loader = torch.utils.data.DataLoader(
+        dataset=training_set,
+        batch_size=batch_size,
+        shuffle=True)
+
+    test_set_loader = torch.utils.data.DataLoader(
+        dataset=testing_set,
+        batch_size=1,
+        shuffle=False)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    spiking_model = SpikingNet(device, n_time_steps = 128, begin_eval=0)
+    train_many_epochs(spiking_model, device, train_set_loader, test_set_loader)
